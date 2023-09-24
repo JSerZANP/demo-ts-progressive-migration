1d67625d3991525cda22d42f077dfd54c364a899
JSer <69352453+JSerZANP@users.noreply.github.com>
2023-09-24 23:48:03 +0900

    diff

--- src/scripts/migrate.ts
+++ src/scripts/migrate.ts
@@ -2,11 +2,18 @@ import ts from "typescript";
 import { simpleGit } from "simple-git";
 const git = simpleGit();
 
+type Diagnostic = {
+  path: string | undefined;
+  start: number | undefined;
+  length: number | undefined;
+  code: number;
+  message: string | ts.DiagnosticMessageChain;
+};
 function collectTscDiagnostics(
   basePath: string,
   pathToConfig: string,
   extraCompilerOptions?: Record<string, any>
-) {
+): Array<Diagnostic> {
   const { config } = ts.readConfigFile(pathToConfig, ts.sys.readFile);
 
   if (config == null) {
@@ -22,7 +29,6 @@ function collectTscDiagnostics(
     ts.sys,
     basePath
   );
-  console.log(options, fileNames);
   const program = ts.createProgram({
     options,
     rootNames: fileNames,
@@ -35,27 +41,44 @@ function collectTscDiagnostics(
     code: diagnostic.code,
     message: diagnostic.messageText,
   }));
-  console.log(diagnostics);
   return diagnostics;
 }
 
-function getAllDiagnostics() {
-  return collectTscDiagnostics(__dirname + "/../../", "./tsconfig.json", {
+function getAllDiagnostics(basePath: string) {
+  return collectTscDiagnostics(basePath, "./tsconfig.json", {
     noImplicitAny: true,
   });
 }
 
-async function diffTscDiagnostics(from: string, to: string) {
+async function diffTscDiagnostics(from: string, to: string, basePath: string) {
   const currentBranch = (await git.branch()).current;
   git.checkout(from);
-  const diagnosticsFrom = getAllDiagnostics();
+  const diagnosticsFrom = getAllDiagnostics(basePath);
   git.checkout(to);
-  const diagnosticsTo = getAllDiagnostics();
+  const diagnosticsTo = getAllDiagnostics(basePath);
   git.checkout(currentBranch);
-  console.log(diagnosticsFrom, diagnosticsTo);
+
+  // we need to filter out new issues and existing issues in diff list
+  const existingDiagnostics = new Map<string, Diagnostic>();
+  diagnosticsFrom.forEach((diagnostic) => {
+    const hash = JSON.stringify(diagnostic);
+    existingDiagnostics.set(hash, diagnostic);
+  });
+
+  const diff = await git.diffSummary([from, to]);
+  const changedFiles = new Set(diff.files.map((file) => basePath + file.file));
+
+  const diagnosticsToFix = diagnosticsTo.filter((diagnostic) => {
+    if (diagnostic.path == null) return false;
+    const hash = JSON.stringify(diagnostic);
+    return !existingDiagnostics.has(hash) || changedFiles.has(diagnostic.path);
+  });
+
+  return diagnosticsToFix;
 }
 
 diffTscDiagnostics(
   "b22d13f06d5d25cab802dd432168f1a604bf8fbc",
-  "f0d9616baddd2dfafffa6d51d243e09c4be9e614"
-);
+  "f0d9616baddd2dfafffa6d51d243e09c4be9e614",
+  __dirname + "/../../"
+).then(console.log);
